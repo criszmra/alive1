96738c0a07ee2e92136efb870363490114414b6f
criszmra <55114216+criszmra@users.noreply.github.com>
2020-03-24 18:50:50 -0600

    avances

--- level 1/circulo .py
+++
@@ -1,9 +0,0 @@
-
-#inputs
-radio = int(input("Medida radio:"))
-#proceso
-
-
-c = radio**2 * 3.14
-#outputs
-print  (f"Este es el radio de tu circulo: {c}")
--- level 1/edad.py
+++
@@ -1,9 +0,0 @@
-# inputs
-
-print ("Programa que te da tu edad")
-fecha = int(input("Inserta tu año de nacimiento: "))
-
-#proceso
-
-resultado = (2019 - fecha)
-print (f"Esta es tu edad: {resultado}")
