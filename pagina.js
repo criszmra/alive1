18693728c15331166ff04ae85f486333d8bd991d
criszmra <55114216+criszmra@users.noreply.github.com>
2020-06-29 21:47:25 -0500

    final

--- pagina.js
+++ pagina.js
@@ -1,15 +1,15 @@
-function sumarlike(){
+function sumarlike(id){
 
-  let nLikes = document.getElementById('nlikes').innerHTML;
+  let nLikes = document.getElementById(id).innerHTML;
   console.log(nLikes);
   let total = parseInt(nLikes)+1
-  document.getElementById('nlikes').innerHTML = total
+  document.getElementById(id).innerHTML = total
 }
 
-function sumardislike(){
+function sumardislike(id){
 
-  let nDislikes = document.getElementById('ndislikes').innerHTML;
+  let nDislikes = document.getElementById(id).innerHTML;
   console.log(nDislikes);
   let total = parseInt(nDislikes)+1
-  document.getElementById("ndislikes").innerHTML = total
+  document.getElementById(id).innerHTML = total
 }
